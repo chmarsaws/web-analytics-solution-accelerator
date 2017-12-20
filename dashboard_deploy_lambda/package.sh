156bb35af0e03c45b10497588ea27aecae227c2d
chris-dev <chrismarshall@optonline.net>
2017-12-20 11:47:56 -0500

    added us-west-1

--- dashboard_deploy_lambda/package.sh
+++ dashboard_deploy_lambda/package.sh
@@ -2,4 +2,4 @@ zip -r dashboard_deploy.zip dashboard_deploy.js node_modules/
 aws s3 cp dashboard_deploy.zip s3://chmars-data-us-east-1/code/dashboard_deploy.zip
 aws s3 cp dashboard_deploy.zip s3://chmars-data-us-west-2/code/dashboard_deploy.zip
 aws s3 cp dashboard_deploy.zip s3://chmars-data-eu-west-1/code/dashboard_deploy.zip
-aws s3 cp dashboard_deploy.zip s3://chmars-data-eu-west-2/code/dashboard_deploy.zip
+#aws s3 cp dashboard_deploy.zip s3://chmars-data-eu-west-2/code/dashboard_deploy.zip
